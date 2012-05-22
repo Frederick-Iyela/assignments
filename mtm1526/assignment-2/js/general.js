efa7f7add8ef13bdb18733b95d12a148fdbd1228
Frederick-Iyela <fbiyela@gmail.com>
2012-05-21 22:34:05 -0400

    Created event listener function.

---
+++ mtm1526/assignment-2/js/general.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+var addBtn = document.getElementById('add-btn');
+
+var bind = function(elem, ev, func){
+	if(window.addEventListener){
+		elem.addEventListener(ev, func, false);
+	}
+	else{
+		elem.attachEvent('on' + ev, func);
+	}
+};
