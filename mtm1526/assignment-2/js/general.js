ba44e97d307d22c1c55be9d0d7e25d497df75734
Frederick-Iyela <fbiyela@gmail.com>
2012-05-25 00:39:50 -0400

    Added functionality to input text into list by pressing 'Enter' or 'Return';

--- mtm1526/assignment-2/js/general.js
+++ mtm1526/assignment-2/js/general.js
@@ -2,7 +2,6 @@
 
 var addBtn = document.getElementById('add-btn');
 var ulList = document.getElementById('todo-list');
-var listForm = document.getElementById('list-form');
 var entry;
 var listEntry = document.getElementById('item');
 
@@ -15,13 +14,38 @@ var bind = function(elem, ev, func){
 	}
 };
 
+var removeCurrent = function() {
+
+	var todoEntry = document.getElementsByTagName('li');
+	var total = todoEntry.length;
+
+	for(var i = 0; i < total; i++){
+
+		todoEntry[i].className = '';
+	}
+};
+
 bind(addBtn, 'click', function(ev){
 	entry = document.createElement('li');
 	
 	if(listEntry){
-		ulList.appendChild(entry)
-		entry.innerHTML = listEntry.value;
+		ulList.appendChild(entry);
+		entry.innerHTML = '<a>' + listEntry.value + '</a>';
 		listEntry.value = '';
+		entry.className = '';
 	}
 	
 });
+
+bind(listEntry, 'change', function(ev){
+	entry = document.createElement('li');
+	
+	if(listEntry){
+		ulList.appendChild(entry);
+		entry.innerHTML = '<a>' + listEntry.value + '</a>';
+		listEntry.value = '';
+		entry.className = '';
+	}
+	
+});
+
