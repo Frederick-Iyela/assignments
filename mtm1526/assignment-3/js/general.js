145d7eab4ee9c3a603b43bb96735a4dbbb95023e
Frederick-Iyela <fbiyela@gmail.com>
2012-05-25 12:07:47 -0400

    Added form-processing functionality.

--- mtm1526/assignment-3/js/general.js
+++ mtm1526/assignment-3/js/general.js
@@ -1,1 +1,24 @@
 // JavaScript Document
+
+$(document).ready(function() {
+	$(form).on('submit', function(ev) {
+		
+		ev.preventDefault();
+		
+		var chosenColour = $('#color').val();
+		
+		switch($('#property').val()){
+			case 'Background Colour':
+				$('.circle').css('background-color', chosenColour);
+			break;
+			
+			case 'Border Colour':
+			break;
+			
+			case 'Colour':
+			break;
+		}
+		
+	});
+	
+});
