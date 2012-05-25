5b7fca11ab7b29c686e61d4ae1e8ed0d0fce86bd
Frederick-Iyela <fbiyela@gmail.com>
2012-05-24 23:38:35 -0400

    Added function that adds text input into list.

--- mtm1526/assignment-2/js/general.js
+++ mtm1526/assignment-2/js/general.js
@@ -1,6 +1,10 @@
 // JavaScript Document
 
 var addBtn = document.getElementById('add-btn');
+var ulList = document.getElementById('todo-list');
+var listForm = document.getElementById('list-form');
+var entry;
+var listEntry = document.getElementById('item');
 
 var bind = function(elem, ev, func){
 	if(window.addEventListener){
@@ -10,3 +14,14 @@ var bind = function(elem, ev, func){
 		elem.attachEvent('on' + ev, func);
 	}
 };
+
+bind(addBtn, 'click', function(ev){
+	entry = document.createElement('li');
+	
+	if(listEntry){
+		ulList.appendChild(entry)
+		entry.innerHTML = listEntry.value;
+		listEntry.value = '';
+	}
+	
+});
