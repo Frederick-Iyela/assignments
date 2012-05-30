f1feddade57e331ab6248e082b502c0c0abc9d8c
Frederick-Iyela <fbiyela@gmail.com>
2012-05-30 19:55:43 -0400

    Gave form-elements classes, so as to style them.

--- mtm1531/assignment-3/index.php
+++ mtm1531/assignment-3/index.php
@@ -13,30 +13,38 @@
 		<div class="container">
 			<form method="post" action="index.php">
 				<div>
-					<label for="name">Name: </label>
+					<div class="container-3">
+						<label for="name">Name: </label>
+					</div>
 					<input type="text" id="name" name="name" required value="<?php echo $name; ?>">
 					<?php if (isset($errors['name'])): ?><strong class="error"> * required</strong><?php endif; ?>
 				</div>
 				
 				<div>
-					<label for="email">Email: </label>
+					<div class="container-3">
+						<label for="email">Email: </label>
+					</div>
 					<input type="email" id="email" name="email" required value="<?php echo $email; ?>">
-					<?php if (isset($errors['email'])): ?><strong class="error"> * required</strong><?php endif; ?>
+					<?php if (isset($errors['email'])): ?><strong class="error"> * required (e.g. you@example.com)</strong><?php endif; ?>
 				</div>
 				
 				<div>
-					<label for="username">Username: </label>
+					<div class="container-3">
+						<label for="username">Username: </label>
+					</div>
 					<input type="text" id="username" name="username" required value="<?php echo $username; ?>">
-					<?php if (isset($errors['username'])): ?><strong class="error"> * required, and must be 5 to 25 characters long</strong><?php endif; ?>
-				</div>
+					<?php if (isset($errors['username'])): ?><strong class="error"> * must be 5 to 25 characters long</strong><?php endif; ?>
+				</div> 
 				
 				<div>
-					<label for="password">Password: </label>
+					<div class="container-3">
+						<label for="password">Password: </label>
+					</div>
 					<input type="password" id="password" name="password" required value="<?php echo $password; ?>">
-					<?php if (isset($errors['password'])): ?><strong class="error"> * required, and must be 5 to 16 characters long</strong><?php endif; ?>
+					<?php if (isset($errors['password'])): ?><strong class="error"> * must be 5 to 16 characters long</strong><?php endif; ?>
 				</div>
 				
-				<fieldset>
+				<fieldset id="box">
 					<legend>Preferred Language</legend>
 					
 					<input type="radio" id="english" name="preferredlang" value="english">
@@ -50,11 +58,13 @@
 				</fieldset>
 				
 				<div>
-					<label for="notes">Notes</label>
+					<div class="container-3">
+						<label for="notes">Notes</label>
+					</div>
 					<textarea id="notes" name="notes"><?php echo $notes; ?></textarea>
 				</div>
 				
-				<div>
+				<div class="container-2">
 					<input type="checkbox" id="acceptterms" name="acceptterms" required value="1">
 					<label for="acceptterms">Accept Terms</label>
 					<?php if (isset($errors['acceptterms'])): ?><strong class="error"> *Terms must be accepted!</strong><?php endif; ?>
