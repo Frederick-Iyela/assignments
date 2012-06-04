833851d94afee36ece0f56320add5ee8be2e595a
Frederick-Iyela <fbiyela@gmail.com>
2012-06-04 15:46:27 -0400

    Created link to database.

--- mtm1531/assignment-5/includes/db.php
+++ mtm1531/assignment-5/includes/db.php
@@ -1,10 +1,9 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+<?php
+	$user = getenv('DB_USER'); 
+	$pass = getenv('DB_PASS'); 
+	$data_source = getenv('DATA_SOURCE');
+	
+	$db = new PDO($data_source, $user, $pass);
+	
+	$db->exec('SET NAMES utf8');
+?>
