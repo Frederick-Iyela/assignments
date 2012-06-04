031a3703e47e396e55ac3fb58776feffb19c6e6f
Frederick-Iyela <fbiyela@gmail.com>
2012-06-04 14:48:40 -0400

    Made changes to index.php file.

--- dinosaurs/index.php
+++ dinosaurs/index.php
@@ -1,3 +1,17 @@
+<?php
+	require_once 'includes/db.php';
+	
+	$sql = $db->query('
+		SELECT id, dino_name, loves_meat, in_jurassic_park
+		FROM dinosaurs
+		ORDER BY dino_name ASC
+	');
+	
+	//var_dump($db->errorInfo());
+	
+	$results = $sql->fetchAll();
+?>
+
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -6,12 +20,14 @@
 	</head>
 	
 	<body>
-		<h2>Velociraptor</h2>
+		<?php foreach($results as $dino):  ?>
+		<h2><a href="single.php?id=<?php echo $dino['id']; ?>"><?php echo $dino['dino_name']; ?></a></h2>
 		<dl>
-			<dt>Loves Meat</dt>
-			<dd>1</dd>
+			<dt>Loves meat</dt>
+			<dd><?php echo $dino['loves_meat']; ?></dd>
 			<dt>In Jurassic Park</dt>
-			<dd>1</dd>
+			<dd><?php echo $dino['in_jurassic_park']; ?></dd>
 		</dl>
+		<?php endforeach; ?>
 	</body>
 </html>
