8108c580993e1098aeb46d5b819f3b4b98a04a4e
Frederick-Iyela <fbiyela@gmail.com>
2012-06-04 15:45:11 -0400

    Modified file and extracted data from database.

--- mtm1531/assignment-5/index.php
+++ mtm1531/assignment-5/index.php
@@ -1,5 +1,15 @@
 <?php
+	require_once 'includes/db.php';
 	
+	$sql = $db->query('
+		SELECT id, movie_title, release_date, director
+		FROM movies
+		ORDER BY movie_title ASC
+	');
+	
+	//var_dump($db->errorInfo());
+	
+	$results = $sql->fetchAll();
 ?>
 
 <!DOCTYPE HTML>
@@ -10,6 +20,16 @@
 	</head>
 	
 	<body>
-		
+		<?php foreach($results as $film):  ?>
+		<h2><a href="single.php?id=<?php echo $film['id']; ?>"><?php echo $film['movie_title']; ?></a></h2>
+		<dl>
+			<dt>Release Date</dt>
+			<dd><?php echo $film['release_date']; ?></dd>
+			
+			<dt>Director</dt>
+			<dd><?php echo $film['director']; ?></dd>
+			
+		</dl>
+		<?php endforeach; ?>
 	</body>
 </html>
