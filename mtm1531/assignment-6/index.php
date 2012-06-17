613b7b82f9b68fd00a801f153d47fa7d498e8eb3
Frederick-Iyela <fbiyela@gmail.com>
2012-06-16 21:34:52 -0400

    Added code to retrieve content from database.

--- mtm1531/assignment-6/index.php
+++ mtm1531/assignment-6/index.php
@@ -1,3 +1,18 @@
+<?php
+	require_once 'includes/db.php';
+	
+	$sql = $db->query('
+		SELECT id, movie_title, release_date, director
+		FROM movies
+		ORDER BY movie_title ASC
+	');
+	
+	//var_dump($db->errorInfo());
+	
+	$results = $sql->fetchAll();
+?>
+
+
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -6,5 +21,19 @@
 	</head>
 	
 	<body>
+		<a href="add.php">Add New Movie</a>
+	
+		<?php foreach($results as $film):  ?>
+		<h2><a href="single.php?id=<?php echo $film['id']; ?>"><?php echo $film['movie_title']; ?></a></h2>
+		<dl>
+			<dt>Release Date</dt>
+			<dd><?php echo $film['release_date']; ?></dd>
+			
+			<dt>Director</dt>
+			<dd><?php echo $film['director']; ?></dd>
+			
+		</dl>
+		<?php endforeach; ?>
+
 	</body>
 </html>
