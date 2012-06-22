86aeb1321bc316c69f662afb76b0d79fbb528daf
Frederick-Iyela <fbiyela@gmail.com>
2012-06-22 01:07:07 -0400

    Added functionality to change <li> classes.

--- mtm1526/assignment-4/js/ajax-tabs.js
+++ mtm1526/assignment-4/js/ajax-tabs.js
@@ -1,1 +1,58 @@
 // JavaScript Document
+
+$(document).ready(function(){
+	
+	var entry;
+	
+	var checkEntry;
+	
+	$.get('earth.html', function(data){
+		
+		$('#fillme').load('earth.html');
+
+	});
+
+	$('#earth').on('click', function(ev){
+		
+		checkEntry = ev.target;
+		checkEntry.className = 'current';
+		
+		$('#fillme').load('earth.html');
+
+	});
+	
+	$('#jupiter').on('click', function(ev){
+		removeCurrent();
+		checkEntry = ev.target;
+		checkEntry.className = 'current';
+		
+		$('#fillme').load('jupiter.html');
+
+	});
+
+	$('#mars').on('click', function(ev){
+		removeCurrent();
+		checkEntry = ev.target;
+		checkEntry.className = 'current';
+		
+		$('#fillme').load('mars.html');
+
+	});
+
+
+	$('#venus').on('click', function(ev){
+		removeCurrent();
+		checkEntry = ev.target;
+		checkEntry.className = 'current';
+	
+		$('#fillme').load('venus.html');
+		
+	});
+	
+	
+	var removeCurrent = function() {
+
+		checkEntry.className = '';
+	};
+	
+});
