ac85a62947556351af9962638aa060984515c44a
Frederick-Iyela <fbiyela@gmail.com>
2012-07-05 22:32:58 -0400

    Added functionality to load pages using javascript.

--- mtm1526/assignment-6/js/planet-tabs.js
+++ mtm1526/assignment-6/js/planet-tabs.js
@@ -1,11 +1,32 @@
 // JavaScript Document
 
 $(document).ready(function(){
-	$('#earth').on('click', function(e){
-		e.preventDefault();
-		$('#fillme').load('includes/earth.php');
+	$('#earth').on('click', function(ev){
+		ev.preventDefault();
+		$('#fillme').load('../includes/earth.php');
 		$('nav li').removeClass('current');
 		$('#earth').addClass('current');
+	});
+	
+	$('#mars').on('click', function(ev){
+		ev.preventDefault();
+		$('#fillme').load('includes/mars.php');
+		$('nav li').removeClass('current');
+		$('#mars').addClass('current');
+	});
+	
+	$('#jupiter').on('click', function(ev){
+		ev.preventDefault();
+		$('#fillme').load('includes/jupiter.php');
+		$('nav li').removeClass('current');
+		$('#jupiter').addClass('current');
+	});
+	
+	$('#venus').on('click', function(ev){
+		ev.preventDefault();
+		$('#fillme').load('includes/venus.php');
+		$('nav li').removeClass('current');
+		$('#venus').addClass('current');
 	});
 	
 });
